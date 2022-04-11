4f7127f1b9f6946a48e78687950d3c97a95d85f0
jpelephant <yuto.20020410@gmail.com>
2022-04-11 11:37:36 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: :customers do
+    root to: 'homes#top'
+  end
+  namespace :admin do
+  end
+
 end
