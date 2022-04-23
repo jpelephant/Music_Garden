0f98861514e57c87463a2444e8549d44ffa53903
jpelephant <yuto.20020410@gmail.com>
2022-04-23 08:51:33 +0000

    [Add]customerのマイページ作成

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,6 +1,7 @@
 class Customer::CustomersController < ApplicationController
   
   def show
+    @customer = Customer.find(params[:id])
   end
   
   def edit 
@@ -9,4 +10,9 @@ class Customer::CustomersController < ApplicationController
   def update
   end
   
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :nickname, :birthday)
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,15 @@ class Customer < ApplicationRecord
          
   has_many :reviews
   has_many :favorites, dependent: :destroy
+  
+  
+  def full_name
+    self.last_name + " " + self.first_name
+  end
+
+  def full_name_kana
+    self.last_name_kana + " " + self.first_name_kana
+  end
+
 end
+
