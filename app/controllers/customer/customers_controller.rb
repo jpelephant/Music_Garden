5d1dd18242cffc4d1298804f0433698ed5faac6a
jpelephant <yuto.20020410@gmail.com>
2022-04-24 07:13:47 +0000

    [Add]customerの編集機能の追加

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -5,9 +5,13 @@ class Customer::CustomersController < ApplicationController
   end
   
   def edit 
+    @customer = Customer.find(params[:id])
   end
   
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to customer_path(@customer.id)
   end
   
   private
