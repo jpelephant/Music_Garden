7a8bc461470c3ff2f25ced06d0b616eacee787d7
jpelephant <yuto.20020410@gmail.com>
2022-04-26 08:06:25 +0000

    [Add]adminのマイページ作成

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,10 +1,20 @@
 class Admins::AdminController < ApplicationController
   def show
+    @admin = Admin.find(params[:id])
   end
 
   def edit
+    @admin = Admin.find(params[:id])
   end
 
   def update
+    @admin = Admin(params[:id])
+    @admin.update(admin_params)
+    redirect_to admin_path(@admin.id)
+  end
+  
+  private
+  def admin_params
+    params.require(:admin).permit(:email, :nickname, :birthday)
   end
 end
