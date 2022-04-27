40cf5fcde809675c66437d4e20f512b4c17debad
jpelephant <yuto.20020410@gmail.com>
2022-04-27 06:22:24 +0000

    [Add]admin/customerの一覧、編集機能の追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,15 +1,17 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
+    @customers = Customer.all
   end
-  
-  def show
-  end
-  
-  def edit
-  end
-  
+
   def update
+    customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customers_path
   end
-  
+
+  private
+  def customer_params
+    params.require(:customer).permit(:nickname)
+  end
 end
