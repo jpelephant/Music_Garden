a0c3dc4fdb79c0793d4b548be7c1ebf2aa29605c
jpelephant <yuto.20020410@gmail.com>
2022-04-28 08:26:04 +0000

    [Add]update destroy アクションの追加

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -26,9 +26,15 @@ class Admin::ReviewsController < ApplicationController
   end
 
   def update
+    review = Review.find(params[:id])
+    review.update(review_params)
+    redirect_to admin_review_path(review)
   end
 
   def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    redirect_to admin_reviews_path
   end
 
   def search
